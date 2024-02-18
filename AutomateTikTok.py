5684d61e79de57c3f97f17dd081bafeb9e467b61
gabymaybe <139580224+gabymaybe@users.noreply.github.com>
2024-02-18 16:07:35 -0500

    Update AutomateTikTok.py to stop leaking my file and session IDS

--- AutomateTikTok.py
+++ AutomateTikTok.py
@@ -8,7 +8,7 @@ import moviepy
 from moviepy.editor import *
 from moviepy.config import change_settings
 import random 
-change_settings({"IMAGEMAGICK_BINARY": "/opt/homebrew/Cellar/imagemagick/7.1.1-24/bin/convert"}) # Fixed imageMagick error
+change_settings({"IMAGEMAGICK_BINARY": ""}) # Fixed imageMagick error
 import whisper_timestamped
 import re
 import subprocess
@@ -16,11 +16,11 @@ from mutagen.mp3 import MP3
 import elevenlabs
 import requests
 # Reddit API, this is the first step, web scraping 
-secret = 'e6Ges3MV7gJKGMwIWQeM7oNbLgH2Eg'
-client_id = 'jUbb6_7qhELN-KR2VESchQ'
-client_user = 'Medium-Tourist3232'
-user_agent = 'MacOS:Med-Tourist:1.0 (by u/Medium-Tourist3232)'
-password = 'gabe1234'
+secret = ''
+client_id = ''
+client_user = ''
+user_agent = ''
+password = ''
 
 reddit = praw.Reddit(
     client_id= client_id,
@@ -43,11 +43,11 @@ def AITAHdata(submission_limit): # Webscrapes the data from reddit
         post_body = submission.selftext
         Post_bodies.append(post_body)
    
-    with open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/Titles.txt','w') as file:
+    with open('') as file:
         for item in post_titles:
             file.write(item + '\n')
        
-    with open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/comments.txt','w') as file:
+    with open('') as file:
         for item in Post_bodies:
             file.write(item + '\n')
             
@@ -68,24 +68,24 @@ def iterateinfo(submission_limit,comment_limit):
         for comment in comments:
             comment_word = comment.body
             top_comments.append(comment_word)
-    file = open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/Titles.txt','w') # could use 'with', but not sure if it will work
+            file = open('') # could use 'with', but not sure if it will work
     for item in post_titles:
         try:
             file.write(item + '\n')
         finally:  
             file.close()
-    file = open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/comments.txt','w')
+    file = open('')
     for item in top_comments:
         try: 
             file.write(item + '\n')
         finally:
             file.close()
             
-file_titles = open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/Titles.txt','r', errors='ignore', encoding='utf-8').read() 
-file_comments = open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/filteredcomment.txt', 'r', errors='ignore', encoding='utf-8').read()
+file_titles = open('', errors='ignore', encoding='utf-8').read() 
+file_comments = open('', 'r', errors='ignore', encoding='utf-8').read()
 
 def remove_double(): # have to use on AITAH 
-    file_path = '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/comments.txt'
+    file_path = ''
     
     # Open the file in read mode and read the content
     with open(file_path, 'r') as file:
@@ -98,22 +98,9 @@ def remove_double(): # have to use on AITAH
     with open(file_path, 'w') as file:
         file.write(content)
 
-# def filter_for_length():
-#     input_file_path = '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/filteredcomment.txt'
-#     output_file_path = '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/readyfortts.txt'
-
-#     with open(input_file_path, 'r') as file, open(output_file_path, 'w') as output_file:
-#         for line in file:
-#             # Remove leading and trailing whitespace
-#             line = line.strip()
-
-#             # Split the line into chunks of 200 characters
-#             for i in range(0, len(line), 200):
-#                 chunk = line[i:i+200]
-#                 output_file.write(chunk + '\n')
 def filter_for_length():
-    input_file_path = '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/filteredcomment.txt'
-    output_file_path = '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/readyfortts.txt'
+    input_file_path = ''
+    output_file_path = ''
 
     with open(input_file_path, 'r') as file, open(output_file_path, 'w') as output_file:
         for line in file:
@@ -137,23 +124,21 @@ def filter_for_length():
 def filter_for_punc():
     pattern = r'https?://\S+|\u200B'
     filtered_comments = []
-    with open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/comments.txt', 'r') as file:
+    with open('') as file:
         for line in file:
             clean_comment = re.sub(pattern, '', line) # only use line.strip if AITAH
             filtered_comments.append(clean_comment)
 
-    with open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/comments.txt', 'w') as file:
+    with open('') as file:
         for comment in filtered_comments:
             file.write(comment)
-    # So basically Im kinda lazy so like IF using ASKREDDIT USE THIS ONE UNDER
-    #open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/filteredcomment.txt','w').writelines(line for line in open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/comments.txt') if 150 < len(line) < 200)
-    # IF using AITAH use this one!!
-    open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/filteredcomment.txt','w').writelines(line for line in open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/comments.txt'))
+   
+    open('').writelines(line for line in open(''))
 
 
 def AITA_replace_jargon():
    # Read the file
-    file_path = '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/readyfortts.txt'
+    file_path = ''
     with open(file_path, 'r') as file:
         content = file.read()
 
@@ -170,7 +155,7 @@ def AITA_replace_jargon():
         file.write(content)
         
         
-    file_path2 = '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/Titles.txt'
+    file_path2 = ''
     
     with open(file_path2, 'r') as file:
         content = file.read()
@@ -188,54 +173,22 @@ def AITA_replace_jargon():
         file.write(content)
         
 def tts_on_title():  
-    with open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/Titles.txt', 'r', errors='ignore', encoding='utf-8') as file:
+    with open('', errors='ignore', encoding='utf-8') as file:
         for line_number, line in enumerate(file, start=0):
-            file_out_name = f"/Users/gabemeredith/Code/python /tiktok-voice-main/TTS/non-combined/title{line_number}.mp3"
-            tts(session_id='3a7cca57c0e729bfa4e40a5615de3765', text_speaker='en_us_006', req_text=line.strip(), filename=file_out_name, play=False)
+            file_out_name = f""
+            tts(session_id='3', text_speaker='en_us_006', req_text=line.strip(), filename=file_out_name, play=False)
 
 
-# def text_to_speech_elevenlabs():
-#     # Read text from the file
-#     try:
-#         with open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/combined.txt', 'r') as file:
-#             text = file.read()
-#     except FileNotFoundError:
-#         return "File not found."
-#     api_key = "c7a77a571798f38979434f83a3e400cd"
 
-#     # Set up the API endpoint and headers
-#     url = "https://api.elevenlabs.io/speech"
-#     headers = {
-#         'Authorization': f'Bearer {api_key}',
-#         'Content-Type': 'application/json'
-#     }
-
-#     # Set up the data payload
-#     data = {
-#         "text": text,
-#         "voice": "premade/Adam"
-#     }
-
-#     # Make the request to ElevenLabs API
-#     response = requests.post(url, json=data, headers=headers)
-
-#     if response.status_code == 200:
-#         # Assuming the API returns a URL to the audio file
-#         audio_url = response.json().get('audioUrl')
-#         return audio_url
-#     else:
-#         return "Error in text-to-speech conversion."
-
-    
 def tts_on_multi():
     # Use 'with' statement for better handling of file opening/closing
-    with open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/readyfortts.txt', 'r', errors='ignore', encoding='utf-8') as file:
+    with open('', errors='ignore', encoding='utf-8') as file:
         for line_number, line in enumerate(file, start=0):
-            file_out_name = f"/Users/gabemeredith/Code/python /tiktok-voice-main/TTS/non-combined/comments{line_number}.mp3"
-            tts(session_id='3a7cca57c0e729bfa4e40a5615de3765', text_speaker='en_us_006', req_text=line.strip(), filename=file_out_name, play=False)
+            file_out_name = f""
+            tts(session_id='', text_speaker='en_us_006', req_text=line.strip(), filename=file_out_name, play=False)
 
 def combine_mp3s():
-    directory = '/Users/gabemeredith/Code/python /tiktok-voice-main/TTS/non-combined'
+    directory = ''
     mp3_files = [f for f in os.listdir(directory) if f.endswith('.mp3')]
     
     def extract_number(file_name):
@@ -253,12 +206,12 @@ def combine_mp3s():
 
     for sound in sounds:
         combined += sound
-    combined.export("/Users/gabemeredith/Code/python /tiktok-voice-main/TTS/Completed/Combined.mp3", format="mp3")
+    combined.export("", format="mp3")
     
     
     
 def clean_dir():
-    files = glob.glob('/Users/gabemeredith/Code/python /tiktok-voice-main/TTS/non-combined/*.mp3')
+    files = glob.glob('')
     for f in files:
         os.remove(f)
         
@@ -273,8 +226,8 @@ def create_combine():
         return words
 
 # Read and split both files
-    words_file1 = read_and_split('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/Titles.txt')
-    words_file2 = read_and_split('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/readyfortts.txt')
+    words_file1 = read_and_split('')
+    words_file2 = read_and_split('')
 
 # Combine the words from both files
     combined_words = []
@@ -285,16 +238,16 @@ def create_combine():
         
 
 # Write the combined words to a new file
-    with open('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/combined.txt', 'w') as combined_file:
+    with open('') as combined_file:
         combined_file.write(' '.join(combined_words))
 
 # Actually, Imma try whisper to see if it's better
     
 def Create_sub():
     model = whisper_timestamped.load_model('medium') # biggest thing is changing models and test it, maybe on PC? 
-    audio = '/Users/gabemeredith/Code/python /tiktok-voice-main/TTS/Completed/Combined.mp3'
+    audio = ''
     results = whisper_timestamped.transcribe(model,audio)
-    New_gameplay = VideoFileClip('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/60Sgameplay.mp4')
+    New_gameplay = VideoFileClip('')
     subs = []
     subs.append(New_gameplay)
     for segment in results['segments']:
@@ -309,13 +262,13 @@ def Create_sub():
 
      #Create a composite video clip with all the text clips
     clip = CompositeVideoClip(subs)
-    clip.write_videofile("/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/Video_file.mp4", fps=25, codec='libx264')
+    clip.write_videofile("", fps=25, codec='libx264')
 
 def create_audio_on_subs():
-    audioclip = AudioFileClip("/Users/gabemeredith/Code/python /tiktok-voice-main/TTS/Completed/Combined.mp3")
-    clip = VideoFileClip('/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/Video_file.mp4', audio= True)     
+    audioclip = AudioFileClip("")
+    clip = VideoFileClip('', audio= True)     
     clip = clip.set_audio(audioclip)
-    clip.write_videofile("/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/finished.mp4", fps=25, codec='libx264')
+    clip.write_videofile("", fps=25, codec='libx264')
     
     
 def reencode_video():
@@ -326,8 +279,8 @@ def reencode_video():
     input_file (str): Path to the input video file.
     output_file (str): Path where the output video file will be saved.
     """
-    input_file = '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/finished.mp4'
-    output_file = '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/Video_file/ready.mp4'
+    input_file = ''
+    output_file = ''
     try:
         command = [
             'ffmpeg', 
@@ -360,21 +313,21 @@ def automate_audio():
     
     
     ### Visual time
-path_to_script = '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/combined.txt'
+path_to_script = ''
 
 
 
 def make_vid():
-    file_path = "/Users/gabemeredith/Code/python /tiktok-voice-main/TTS/Completed/Combined.mp3" 
+    file_path = "" 
     audio = MP3(file_path)
     duration = audio.info.length
-    gameplay = VideoFileClip("/Users/gabemeredith/Downloads/13 Minutes Minecraft Parkour Gameplay [Free to Use] [Map Download].mp4", audio = False)
+    gameplay = VideoFileClip("", audio = False)
     max_start = max(0, gameplay.duration - duration)
     start_time = random.uniform(0, max_start)
     new_clip = gameplay.subclip(start_time, start_time + duration)
 
     # Save the new clip
-    output_path = '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/60Sgameplay.mp4'
+    output_path = ''
     new_clip.write_videofile(output_path, codec='libx264')
 
     
@@ -388,13 +341,8 @@ def main():
     reencode_video()
     
     
-        #tts(session_id='13dc2c6357484a029686688de60a5f1c8',text_speaker='en_us_006',req_text=file_comments,filename='/Users/gabemeredith/Code/python /tiktok-voice-main/TTS/comments.mp3',play=False)
         
 if __name__ == '__main__':
     main()
    
     
-# python -u "/Users/gabemeredith/Code/python /tiktok-voice-main/main.py" -v 'en_us_006' -f '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/comments.txt' -n '/Users/gabemeredith/Code/python /tiktok-voice-main/TTS/test.mp3' --session '13dc2c6357484a029686688e60a5f1c8'
-# python -u "/Users/gabemeredith/Code/python /tiktok-voice-main/main.py" -v 'en_us_006' -f '/Users/gabemeredith/Code/python /tiktok-voice-main/AutomatingTIkToks/titles.txt' -n '/Users/gabemeredith/Code/python /tiktok-voice-main/TTS/test.mp3' --session '13dc2c6357484a029686688e60a5f1c8'
-
-# Google ACCOUND redditlongbottom@gmail.com PW : ChristainBalestan123
